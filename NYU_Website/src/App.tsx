c2c302ece8718a692d376936c1384c423b1dd399
danctila <danctila@gmail.com>
2023-08-09 19:04:42 -0400

    Created header component

--- NYU_Website/src/App.tsx
+++ NYU_Website/src/App.tsx
@@ -1,9 +1,11 @@
 import InformationBar from "./Components/InformationBar";
+import NYUHeader from "./Components/NYUHeader";
 
 function App() {
   return (
     <>
       <InformationBar />
+      <NYUHeader />
     </>
   );
 }
--- NYU_Website/src/Components/NYUHeader.tsx
+++ NYU_Website/src/Components/NYUHeader.tsx
@@ -1,5 +1,74 @@
+import { HStack, Button, Image, Flex } from "@chakra-ui/react";
+import NYULogo from "..//assets/NYU-Emblem.png";
+
 const NYUHeader = () => {
-  return <div>NYUHeader</div>;
+  return (
+    <HStack bg="#57068C" height="115" width="fit">
+      <Image src={NYULogo} height="72x" width="140px" marginLeft="360px" />
+      <HStack spacing="0" backgroundColor="#57068C">
+        <Button
+          variant="ghost"
+          borderRadius="0px"
+          width="150px"
+          height="115px"
+          color="white"
+          backgroundColor="#57068C"
+          _hover={{ background: "#330662", color: "white" }}
+          fontWeight="normal"
+        >
+          About NYU
+        </Button>
+        <Button
+          variant="ghost"
+          borderRadius="0px"
+          width="150px"
+          height="115px"
+          color="white"
+          backgroundColor="#57068C"
+          _hover={{ background: "#330662", color: "white" }}
+          fontWeight="normal"
+        >
+          Admissions
+        </Button>
+        <Button
+          variant="ghost"
+          borderRadius="0px"
+          width="150px"
+          height="115px"
+          color="white"
+          backgroundColor="#57068C"
+          _hover={{ background: "#330662", color: "white" }}
+          fontWeight="normal"
+        >
+          Academics
+        </Button>
+        <Button
+          variant="ghost"
+          borderRadius="0px"
+          width="180px"
+          height="115px"
+          color="white"
+          backgroundColor="#57068C"
+          _hover={{ background: "#330662", color: "white" }}
+          fontWeight="normal"
+        >
+          University Life
+        </Button>
+        <Button
+          variant="ghost"
+          borderRadius="0px"
+          width="150px"
+          height="115px"
+          color="white"
+          backgroundColor="#57068C"
+          _hover={{ background: "#330662", color: "white" }}
+          fontWeight="normal"
+        >
+          Research
+        </Button>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default NYUHeader;
