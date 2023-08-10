079caf07433fe8a8b3d361dadb90ace514cb728d
danctila <danctila@gmail.com>
2023-08-10 19:04:01 -0400

    Fixed import in main for index.css

--- NYU_Website/src/main.tsx
+++ NYU_Website/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
-import "./index.css";
 import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
