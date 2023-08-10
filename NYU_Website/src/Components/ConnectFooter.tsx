57233672c674799b3918b2f0333258ed0680ab47
danctila <danctila@gmail.com>
2023-08-10 15:50:48 -0400

    Added logo images into connect bar

--- NYU_Website/src/Components/ConnectFooter.tsx
+++ NYU_Website/src/Components/ConnectFooter.tsx
@@ -1,4 +1,17 @@
-import { Box, HStack, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import instagram from "../assets/instagramLogo.png";
+import twitter from "../assets/twitterLogo.png";
+import facebook from "../assets/facebookLogo.png";
+import linkedin from "../assets/linkedinLogo.png";
+import youtube from "../assets/youtubeLogo.png";
 
 const ConnectFooter = () => {
   return (
@@ -14,6 +27,55 @@ const ConnectFooter = () => {
           <Link color="white">Accessibility</Link>
           <Link color="white">Feedback</Link>
         </HStack>
+        <Grid
+          h="200px"
+          w="1200px"
+          margin="auto"
+          padding="5px"
+          templateRows="repeat(1, 1fr)"
+          templateColumns="repeat(3, 1fr)"
+        >
+          <GridItem
+            rowSpan={1}
+            colSpan={1}
+            bg="tomato"
+            borderRight="1px"
+            borderTop="1px"
+            borderColor="white"
+          >
+            <Text
+              fontWeight="semibold"
+              fontSize="20px"
+              color="white"
+              marginTop="20px"
+            >
+              CONNECT WITH NYU
+            </Text>
+            <HStack spacing="20px" paddingTop="10px">
+              <Image src={instagram} alt="instagram" boxSize="20px" />
+              <Image src={twitter} alt="twitter" boxSize="20px" />
+              <Image src={facebook} alt="facebook" boxSize="20px" />
+              <Image src={linkedin} alt="linkedin" boxSize="20px" />
+              <Image src={youtube} alt="youtube" height="18px" width="25px" />
+            </HStack>
+          </GridItem>
+          <GridItem
+            rowSpan={1}
+            colSpan={1}
+            bg="tomato"
+            width="600px"
+            borderTop="1px"
+            borderColor="white"
+          />
+          <GridItem
+            rowSpan={1}
+            colSpan={1}
+            bg="tomato"
+            borderLeft="1px"
+            borderTop="1px"
+            borderColor="white"
+          />
+        </Grid>
       </Box>
     </>
   );
